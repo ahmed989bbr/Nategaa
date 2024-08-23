8bd3b51e72abc01cefdf6f915ec3232f122daa3a
ahmed989bbr <107225916+ahmed989bbr@users.noreply.github.com>
2024-08-24 01:05:19 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div className="content">
 
       <Routes>
-        <Route path='/' element={<Resalt/>} />
+        <Route path='/*' element={<Resalt/>} />
         <Route path='/upload' element={<Xlec/>} />
       </Routes>
     </div>
